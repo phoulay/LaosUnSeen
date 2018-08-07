63fdf9ffa92ce2c9cf8d9bbb5f868c130d2347d9
masterUNG <phrombutr@gmail.com>
2018-08-07 15:54:19 +0700

    Update 7AugV4

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -1,5 +1,7 @@
 package masterung.androidthai.in.th.laosunseen.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,17 +10,50 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import masterung.androidthai.in.th.laosunseen.MainActivity;
 import masterung.androidthai.in.th.laosunseen.R;
 
 public class RegisterFragment extends Fragment{
 
+    //    Explicit
+    private Uri uri;
+    private ImageView imageView;
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        createToolbar();
+
+//        Photo Controller
+        photoController();
+
+    }   // Main Class
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void photoController() {
+        imageView = getView().findViewById(R.id.imvPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+
+            }
+        });
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -31,8 +66,7 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-    }   // Main Class
+    }
 
     @Nullable
     @Override

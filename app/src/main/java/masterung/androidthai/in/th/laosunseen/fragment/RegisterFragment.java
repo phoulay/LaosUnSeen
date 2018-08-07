810c85070183e096cae821acadb1067bd8ade08e
masterUNG <phrombutr@gmail.com>
2018-08-07 17:05:04 +0700

    Finish 7Aug

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -1,6 +1,8 @@
 package masterung.androidthai.in.th.laosunseen.fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,9 +10,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import masterung.androidthai.in.th.laosunseen.MainActivity;
 import masterung.androidthai.in.th.laosunseen.R;
@@ -35,8 +41,46 @@ public class RegisterFragment extends Fragment{
     }   // Main Class
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 400, 300, true);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Photo", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void photoController() {
@@ -66,6 +110,7 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+        setHasOptionsMenu(true);
     }
 
     @Nullable
